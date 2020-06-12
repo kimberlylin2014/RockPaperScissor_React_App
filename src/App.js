63d74e87bd523845bfcfc9ac0f9597f22dd93572
Kimberly Lin <kimberlylin2014@gmail.com>
2020-06-11 18:23:29 -0700

    completed readMe

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
     return (
       <div className="App">
            <Game />
+           <footer className="fixed mt-5">
+              <p className="text-center" style={{color:"white"}}>Icons made by <a href="https://www.flaticon.com/authors/freepik">Freepik</a>  from  <a href="https://www.flaticon.com">www.flaticon.com</a> </p>
+          </footer>
       </div>
   
     )
